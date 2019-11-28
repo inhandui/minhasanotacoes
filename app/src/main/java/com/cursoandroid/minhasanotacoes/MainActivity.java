be56a1005068af38b77399b6b33f53d0397e583e
Inhandui Barbosa Ferreira <inhandui@gmail.com>
2019-11-28 19:50:28 -0300

    Implementing write file.

--- app/src/main/java/com/cursoandroid/minhasanotacoes/MainActivity.java
+++ app/src/main/java/com/cursoandroid/minhasanotacoes/MainActivity.java
@@ -2,15 +2,22 @@ package com.cursoandroid.minhasanotacoes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText txtAnotacoes;
     private ImageView btnSalvar;
+
+    private static final String ARQUIVO_ANOTACOES = "arqAnotacoes.txt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +31,23 @@ public class MainActivity extends AppCompatActivity {
         btnSalvar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                gravarArquivo(txtAnotacoes.getText().toString());
             }
         });
 
     }
+
+    private void gravarArquivo(String texto) {
+
+        try{
+            OutputStreamWriter outputSW = new OutputStreamWriter( openFileOutput(ARQUIVO_ANOTACOES, Context.MODE_PRIVATE));
+            outputSW.write(texto);
+            outputSW.close();
+
+        }catch (IOException e){
+            Log.v("MainActivity", e.toString());
+        }
+    }
+
+
 }
