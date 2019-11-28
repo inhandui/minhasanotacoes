a6d8af4655e048cff4dcaa6d5ece5d45aae227ea
Inhandui Barbosa Ferreira <inhandui@gmail.com>
2019-11-28 19:35:11 -0300

    Adding elements reference.

--- app/src/main/java/com/cursoandroid/minhasanotacoes/MainActivity.java
+++ app/src/main/java/com/cursoandroid/minhasanotacoes/MainActivity.java
@@ -3,12 +3,23 @@ package com.cursoandroid.minhasanotacoes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText txtAnotacoes;
+    private ImageView btnSalvar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtAnotacoes = findViewById(R.id.txtAnotacoes);
+
+        btnSalvar = findViewById(R.id.btnSalvar);
+
+
     }
 }
